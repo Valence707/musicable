3bad256470abad101badae9c42a5318b69127e33
Valence707 <88735071+Valence707@users.noreply.github.com>
2021-09-16 16:28:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function infoOverlayOn() {
+	document.getElementById("siteInfoOverlayContainer").style.display = "flex";
+  }
+  
+  function infoOverlayOff() {
+	document.getElementById("siteInfoOverlayContainer").style.display = "none";
+  } 
+
+function settingsOverlayOn() {
+  document.getElementById("siteInfoOverlayContainer").style.display = "flex";
+  }
+  
+  function settingsOverlayOff() {
+  document.getElementById("siteInfoOverlayContainer").style.display = "none";
+  } 
