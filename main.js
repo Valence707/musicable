311c1a4cbfab0e6e60ad51d3c4188c3b4a7a635a
valence707 <88735071+Valence707@users.noreply.github.com>
2021-09-28 10:40:25 -0400

    Add files via upload
    Changes:
    > Added JavaScript files for each site.
    > Added persistence of color theme. Color theme now stays the same regardless of whether page is reloaded or tabs are switched. The color theme also remains consistent across the different pages.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+
+
 function overlayBackgroundOn() {
-  document.getElementById('overlayContainer').style.display = 'block';
+  document.querySelector('#overlayContainer').style.display = 'block';
 }
 
 function overlayOff() {
@@ -7,107 +9,128 @@ function overlayOff() {
 }
 
 function infoOverlayOn() {
-  document.getElementById('infoOverlay').style.display = 'flex';
-  document.getElementById("settingsOverlay").style['flex-flow'] = 'column';
-  document.getElementById('settingsOverlay').style.display = 'none';
+  document.querySelector('#infoOverlay').style.display = 'flex';
+  document.querySelector("#settingsOverlay").style['flex-flow'] = 'column';
+  document.querySelector('#settingsOverlay').style.display = 'none';
   overlayBackgroundOn();
   }
 
 function settingsOverlayOn() {
-  document.getElementById("settingsOverlay").style.display = "flex";
-  document.getElementById("settingsOverlay").style['flex-flow'] = 'column';
-  document.getElementById('infoOverlay').style.display = 'none';
+  document.querySelector("#settingsOverlay").style.display = "flex";
+  document.querySelector("#settingsOverlay").style['flex-flow'] = 'column';
+  document.querySelector('#infoOverlay').style.display = 'none';
   overlayBackgroundOn();
   }
 
-let documentColorTheme = 'dark';
 
-function colorThemeChange() {
-  if (documentColorTheme != 'dark') {
 
-    // All dark mode theme methods here.
-    documentColorTheme = 'dark';
-    document.body.style['background-color'] = 'black';
-    document.getElementById('colorThemeIndicatorButton').textContent = 'Dark';
-    document.getElementById('colorThemeIndicatorButton').style.color = 'yellow';
-    document.getElementById('colorThemeIndicatorButton').style['background-color'] = 'black';
-    document.getElementById('backgroundDecorationContainer').style.filter = 'invert(0)';
-    const titleButtonText = document.getElementById('titleButtonContainer').querySelectorAll('h1,p');
-    for (let i = 0; i < titleButtonText.length; i++) {
-      titleButtonText[i].style.color = 'white';
+const saveColorTheme = (color) => {
+  localStorage.setItem('documentColorTheme', color)
+}
 
-    const titleButtonImages = document.getElementById('titleButtonContainer').querySelectorAll('img');
-    for (let i = 0; i < titleButtonImages.length; i++) {
-      titleButtonImages[i].style.filter = 'invert(0)';
-    }
+function darkModeColorTheme() {
+  // All dark mode theme methods here.
+  saveColorTheme('dark');
+  document.body.style['background-color'] = 'black';
+  document.getElementById('colorThemeIndicatorButton').textContent = 'Dark';
+  document.getElementById('colorThemeIndicatorButton').style.color = 'yellow';
+  document.getElementById('colorThemeIndicatorButton').style['background-color'] = 'black';
+  document.getElementById('backgroundDecorationContainer').style.filter = 'invert(0)';
+  const titleButtonText = document.getElementById('titleButtonContainer').querySelectorAll('h1,p');
+  for (let i = 0; i < titleButtonText.length; i++) {
+    titleButtonText[i].style.color = 'white';
+  }
 
-    document.getElementById('toolBar').style['background-color'] = 'black';
-    document.getElementById('toolBar').style['border-color'] = 'yellow';
+  const titleButtonImages = document.getElementById('titleButtonContainer').querySelectorAll('img');
+  for (let i = 0; i < titleButtonImages.length; i++) {
+    titleButtonImages[i].style.filter = 'invert(0)';
+  }
 
-    const toolBarText = document.getElementById('toolBar').querySelectorAll('div');
-    for (let i = 0; i < toolBarText.length; i++) {
-      toolBarText[i].style.color = 'yellow';
-    }
+  document.getElementById('toolBar').style['background-color'] = 'black';
+  document.getElementById('toolBar').style['border-color'] = 'yellow';
 
-    const overlayText = document.getElementById('overlayContainer').querySelectorAll('p');
-    for (let i = 0; i < overlayText.length; i++) {
-      overlayText[i].style.color = 'white';
-    }
+  const toolBarText = document.getElementById('toolBar').querySelectorAll('div');
+  for (let i = 0; i < toolBarText.length; i++) {
+    toolBarText[i].style.color = 'yellow';
+  }
 
-    const overlayBodies = document.getElementById('overlayContainer').querySelectorAll('div');
-    for (let i = 0; i < overlayBodies.length; i++) {
-      overlayBodies[i].style['background-color'] = 'black';
-      overlayBodies[i].style['border-color'] = 'yellow';
-    }
+  const overlayText = document.getElementById('overlayContainer').querySelectorAll('p');
+  for (let i = 0; i < overlayText.length; i++) {
+    overlayText[i].style.color = 'white';
+  }
 
-    const closeButtons = document.querySelectorAll('h2,h3');
-    for (let i = 0; i < closeButtons.length; i++) {
-      closeButtons[i].style.color = 'yellow';
-    }
+  const overlayBodies = document.getElementById('overlayContainer').querySelectorAll('div');
+  for (let i = 0; i < overlayBodies.length; i++) {
+    overlayBodies[i].style['background-color'] = 'black';
+    overlayBodies[i].style['border-color'] = 'yellow';
+  }
+
+  const closeButtons = document.querySelectorAll('h2,h3');
+  for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].style.color = 'yellow';
   }
 }
 
-  else if (documentColorTheme = 'dark') {
+function lightModeColorTheme() {
 
-    // All light mode theme methods here.
-    documentColorTheme = 'light';
-    document.body.style['background-color'] = 'white';
-    document.getElementById('colorThemeIndicatorButton').textContent = 'Light';
-    document.getElementById('colorThemeIndicatorButton').style.color = 'black';
-    document.getElementById('colorThemeIndicatorButton').style['background-color'] = 'white';
-    document.getElementById('backgroundDecorationContainer').style.filter = 'invert(1)';
-    const titleButtonText = document.getElementById('titleButtonContainer').querySelectorAll('h1,p');
-    for (let i = 0; i < titleButtonText.length; i++) {
-      titleButtonText[i].style.color = 'black';
-    }
+  // All light mode theme methods here.
+  saveColorTheme('light');
+  document.body.style['background-color'] = 'white';
+  document.querySelector('#colorThemeIndicatorButton').textContent = 'Light';
+  document.querySelector('#colorThemeIndicatorButton').style.color = 'black';
+  document.querySelector('#colorThemeIndicatorButton').style['background-color'] = 'white';
+  document.querySelector('#backgroundDecorationContainer').style.filter = 'invert(1)';
+  const titleButtonText = document.querySelector('#titleButtonContainer').querySelectorAll('h1,p');
+  for (let i = 0; i < titleButtonText.length; i++) {
+    titleButtonText[i].style.color = 'black';
+  }
 
-    const titleButtonImages = document.getElementById('titleButtonContainer').querySelectorAll('img');
-    for (let i = 0; i < titleButtonImages.length; i++) {
-      titleButtonImages.item(i).style.filter = 'invert(1)';
-    }
+  const titleButtonImages = document.querySelector('#titleButtonContainer').querySelectorAll('img');
+  for (let i = 0; i < titleButtonImages.length; i++) {
+    titleButtonImages.item(i).style.filter = 'invert(1)';
+  }
 
-    document.getElementById('toolBar').style['background-color'] = 'white';
-    document.getElementById('toolBar').style['border-color'] = 'blue';
+  document.querySelector('#toolBar').style['background-color'] = 'white';
+  document.querySelector('#toolBar').style['border-color'] = 'blue';
 
-    const toolBarText = document.getElementById('toolBar').querySelectorAll('div');
-    for (let i = 0; i < toolBarText.length; i++) {
-      toolBarText[i].style.color = 'black';
-    }
+  const toolBarText = document.querySelector('#toolBar').querySelectorAll('div');
+  for (let i = 0; i < toolBarText.length; i++) {
+    toolBarText[i].style.color = 'black';
+  }
 
-    const overlayText = document.getElementById('overlayContainer').querySelectorAll('p');
-    for (let i = 0; i < overlayText.length; i++) {
-      overlayText[i].style.color = 'black';
-    }
+  const overlayText = document.querySelector('#overlayContainer').querySelectorAll('p');
+  for (let i = 0; i < overlayText.length; i++) {
+    overlayText[i].style.color = 'black';
+  }
 
-    const overlayBodies = document.getElementById('overlayContainer').querySelectorAll('div');
-    for (let i = 0; i < overlayBodies.length; i++) {
-      overlayBodies[i].style['background-color'] = 'white';
-      overlayBodies[i].style['border-color'] = 'blue';
-    }
+  const overlayBodies = document.querySelector('#overlayContainer').querySelectorAll('div');
+  for (let i = 0; i < overlayBodies.length; i++) {
+    overlayBodies[i].style['background-color'] = 'white';
+    overlayBodies[i].style['border-color'] = 'blue';
+  }
 
-    const closeButtons = document.querySelectorAll('h2,h3');
-    for (let i = 0; i < closeButtons.length; i++) {
-      closeButtons[i].style.color = 'black';
-    }
+  const closeButtons = document.querySelectorAll('h2,h3');
+  for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].style.color = 'black';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function initializeColorTheme() {
+  var colorTheme = localStorage.getItem('documentColorTheme', localStorage.getItem('documentColorTheme'));
+  if (colorTheme == null || colorTheme == undefined) {
+    darkModeColorTheme();
+  } else if (colorTheme == 'light') {
+    lightModeColorTheme();
+  } else if (colorTheme == 'dark') {
+    darkModeColorTheme();
+  }
+});
+
+function colorThemeChange() {
+  var colorTheme = localStorage.getItem('documentColorTheme');
+  if (colorTheme == 'light') {
+    darkModeColorTheme();
+  } else if (colorTheme == 'dark') {
+    lightModeColorTheme();
   }
 }
---
+++ sharing.js
@@ -0,0 +1,136 @@
+
+
+function overlayBackgroundOn() {
+	document.querySelector('#overlayContainer').style.display = 'block';
+  }
+  
+  function overlayOff() {
+	document.querySelector('#overlayContainer').style.display = 'none';
+  }
+  
+  function infoOverlayOn() {
+	document.querySelector('#infoOverlay').style.display = 'flex';
+	document.querySelector("#settingsOverlay").style['flex-flow'] = 'column';
+	document.querySelector('#settingsOverlay').style.display = 'none';
+	overlayBackgroundOn();
+	}
+  
+  function settingsOverlayOn() {
+	document.querySelector("#settingsOverlay").style.display = "flex";
+	document.querySelector("#settingsOverlay").style['flex-flow'] = 'column';
+	document.querySelector('#infoOverlay').style.display = 'none';
+	overlayBackgroundOn();
+	}
+  
+  
+  
+  const saveColorTheme = (color) => {
+	localStorage.setItem('documentColorTheme', color)
+  }
+  
+  function darkModeColorTheme() {
+	// All dark mode theme methods here.
+	saveColorTheme('dark');
+	document.body.style['background-color'] = 'black';
+	document.getElementById('colorThemeIndicatorButton').textContent = 'Dark';
+	document.getElementById('colorThemeIndicatorButton').style.color = 'yellow';
+	document.getElementById('colorThemeIndicatorButton').style['background-color'] = 'black';
+	document.getElementById('backgroundDecorationContainer').style.filter = 'invert(0)';
+	const titleButtonText = document.getElementById('titleButtonContainer').querySelectorAll('h1,p');
+	for (let i = 0; i < titleButtonText.length; i++) {
+	  titleButtonText[i].style.color = 'white';
+	}
+  
+	const titleButtonImages = document.getElementById('titleButtonContainer').querySelectorAll('img');
+	for (let i = 0; i < titleButtonImages.length; i++) {
+	  titleButtonImages[i].style.filter = 'invert(0)';
+	}
+  
+	document.getElementById('toolBar').style['background-color'] = 'black';
+	document.getElementById('toolBar').style['border-color'] = 'yellow';
+  
+	const toolBarText = document.getElementById('toolBar').querySelectorAll('div');
+	for (let i = 0; i < toolBarText.length; i++) {
+	  toolBarText[i].style.color = 'yellow';
+	}
+  
+	const overlayText = document.getElementById('overlayContainer').querySelectorAll('p');
+	for (let i = 0; i < overlayText.length; i++) {
+	  overlayText[i].style.color = 'white';
+	}
+  
+	const overlayBodies = document.getElementById('overlayContainer').querySelectorAll('div');
+	for (let i = 0; i < overlayBodies.length; i++) {
+	  overlayBodies[i].style['background-color'] = 'black';
+	  overlayBodies[i].style['border-color'] = 'yellow';
+	}
+  
+	const closeButtons = document.querySelectorAll('h2,h3');
+	for (let i = 0; i < closeButtons.length; i++) {
+	  closeButtons[i].style.color = 'yellow';
+	}
+  }
+  
+  function lightModeColorTheme() {
+  
+	// All light mode theme methods here.
+	saveColorTheme('light');
+	document.body.style['background-color'] = 'white';
+	document.querySelector('#colorThemeIndicatorButton').textContent = 'Light';
+	document.querySelector('#colorThemeIndicatorButton').style.color = 'black';
+	document.querySelector('#colorThemeIndicatorButton').style['background-color'] = 'white';
+	document.querySelector('#backgroundDecorationContainer').style.filter = 'invert(1)';
+	const titleButtonText = document.querySelector('#titleButtonContainer').querySelectorAll('h1,p');
+	for (let i = 0; i < titleButtonText.length; i++) {
+	  titleButtonText[i].style.color = 'black';
+	}
+  
+	const titleButtonImages = document.querySelector('#titleButtonContainer').querySelectorAll('img');
+	for (let i = 0; i < titleButtonImages.length; i++) {
+	  titleButtonImages.item(i).style.filter = 'invert(1)';
+	}
+  
+	document.querySelector('#toolBar').style['background-color'] = 'white';
+	document.querySelector('#toolBar').style['border-color'] = 'blue';
+  
+	const toolBarText = document.querySelector('#toolBar').querySelectorAll('div');
+	for (let i = 0; i < toolBarText.length; i++) {
+	  toolBarText[i].style.color = 'black';
+	}
+  
+	const overlayText = document.querySelector('#overlayContainer').querySelectorAll('p');
+	for (let i = 0; i < overlayText.length; i++) {
+	  overlayText[i].style.color = 'black';
+	}
+  
+	const overlayBodies = document.querySelector('#overlayContainer').querySelectorAll('div');
+	for (let i = 0; i < overlayBodies.length; i++) {
+	  overlayBodies[i].style['background-color'] = 'white';
+	  overlayBodies[i].style['border-color'] = 'blue';
+	}
+  
+	const closeButtons = document.querySelectorAll('h2,h3');
+	for (let i = 0; i < closeButtons.length; i++) {
+	  closeButtons[i].style.color = 'black';
+	}
+  }
+  
+  document.addEventListener('DOMContentLoaded', function initializeColorTheme() {
+	var colorTheme = localStorage.getItem('documentColorTheme', localStorage.getItem('documentColorTheme'));
+	if (colorTheme == null || colorTheme == undefined) {
+	  darkModeColorTheme();
+	} else if (colorTheme == 'light') {
+	  lightModeColorTheme();
+	} else if (colorTheme == 'dark') {
+	  darkModeColorTheme();
+	}
+  });
+  
+  function colorThemeChange() {
+	var colorTheme = localStorage.getItem('documentColorTheme');
+	if (colorTheme == 'light') {
+	  darkModeColorTheme();
+	} else if (colorTheme == 'dark') {
+	  lightModeColorTheme();
+	}
+  }
